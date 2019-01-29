1d213721a49fe2777527aaa801ab8852e0e4773d
jayjoshi <jay.joshi@coviam.com>
2019-01-29 14:04:40 +0530

    Initial commit

---
+++ src/main/java/com/recommendation/update_service/UpdateServiceApplication.java
@@ -0,0 +1,14 @@
+package com.recommendation.update_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdateServiceApplication.class, args);
+	}
+
+}
+
