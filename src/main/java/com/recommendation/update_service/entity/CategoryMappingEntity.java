04b8f6847ce34207fbc6326bc94d6e1fcfed05ec
jayjoshi <jay.joshi@coviam.com>
2019-01-29 14:22:31 +0530

    edit category mapping

--- src/main/java/com/recommendation/update_service/entity/CategoryMappingEntity.java
+++ src/main/java/com/recommendation/update_service/entity/CategoryMappingEntity.java
@@ -1,4 +1,40 @@
 package com.recommendation.update_service.entity;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashMap;
+
+@Document(collection = "CategoryMapping")
 public class CategoryMappingEntity {
+
+    @Id
+    private String userId;
+
+    private HashMap<String,Double> categories;
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public HashMap<String, Double> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(HashMap<String, Double> categories) {
+        this.categories = categories;
+    }
+
+    @Override
+    public String toString() {
+        return "CategoryMappingEntity{" +
+                "userId='" + userId + '\'' +
+                ", categories=" + categories +
+                '}';
+    }
 }
